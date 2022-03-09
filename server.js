3e7720724fda791bbb1a23d48a68d9bcaa51c977
ranonsew <ranonsew.2020@scis.smu.edu.sg>
2022-03-10 00:48:13 +0800

    create and update server
    express like flask, to start a HTTP server type thing

---
+++ server.js
@@ -0,0 +1,17 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const index_js_1 = __importDefault(require("./index.js"));
+const app = (0, express_1.default)();
+const PORT = 3002;
+const HOST = "127.0.0.1";
+// to use the microservice: 
+// https://HOST:PORT/v1/sms with a POST request
+// request body should be json, in the format of { "message": "placeholder string" }
+app.use(express_1.default.json())
+    .use(express_1.default.urlencoded({ extended: true }))
+    .use("/v1/sms", index_js_1.default)
+    .listen(PORT, HOST, () => console.log(`App running on http://${HOST}:${PORT}`));
---
+++ server.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import twilioSMS from "./index.js";
+
+const app = express();
+const PORT = 3002;
+const HOST = "127.0.0.1";
+
+// to use the microservice: 
+// https://HOST:PORT/v1/sms with a POST request
+// request body should be json, in the format of { "message": "placeholder string" }
+app.use(express.json())
+  .use(express.urlencoded({ extended: true }))
+  .use("/v1/sms", twilioSMS)
+  .listen(PORT, HOST, () => console.log(`App running on http://${HOST}:${PORT}`));
