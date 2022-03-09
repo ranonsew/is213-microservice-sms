eae9f26d1df722c4f4e485566fd78618927d7b09
ranonsew <ranonsew.2020@scis.smu.edu.sg>
2022-03-10 01:06:22 +0800

    update index
    updated to use object destructuring with process.env rather than individually making them constant variables
    
    extra comments and all that

--- index.js
+++ index.js
@@ -16,18 +16,21 @@ const accountSID = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const twilioNumber = process.env.TWILIO_PHONE_NUMBER;
 const userNumber = process.env.PLACEHOLDER_PHONE_NUMBER; // replace this with the phone number obtained from user information later
+const { TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, TWILIO_PHONE_NUMBER, PLACEHOLDER_PHONE_NUMBER } = process.env;
 // for main post request (sending message to the user)
 router.route('/')
     .post((req, res) => {
     const { message } = req.body;
-    if (!accountSID || !authToken || !twilioNumber || !userNumber)
+    if (!TWILIO_ACCOUNT_SID || !TWILIO_AUTH_TOKEN || !TWILIO_PHONE_NUMBER || !PLACEHOLDER_PHONE_NUMBER)
         res.status(500).json(returnMsg("One or more variables are missing"));
+    // if (!accountSID || !authToken || !twilioNumber || !userNumber) res.status(500).json(returnMsg("One or more variables are missing"));
     else {
-        const twilioClient = new twilio_1.Twilio(accountSID, authToken);
+        const twilioClient = new twilio_1.Twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
+        // const twilioClient = new Twilio(accountSID, authToken);
         twilioClient.messages
             .create({
-            from: twilioNumber,
-            to: userNumber,
+            from: TWILIO_PHONE_NUMBER,
+            to: PLACEHOLDER_PHONE_NUMBER,
             body: message
         })
             .then((msg) => res.status(200).json(returnMsg("Message has been sent to the user")))
@@ -38,22 +41,4 @@ router.route('/')
 // using .all() to target all routes after the initial post request
 router.route('*')
     .all((req, res) => res.status(400).json(returnMsg(msg_invalid)));
-// old stuff that was used before realizing that I did a dumb
-// that dumb being that building the docker container triggered the thing instead of being a microservice
-/*
-const placeholder_msg = "placeholder message to send or something";
-if (!accountSID || !authToken || !twilioNumber || !userNumber) console.error("One or more variables missing");
-else {
-  // if all fields present
-  const twilioClient = new Twilio(accountSID, authToken);
-  twilioClient.messages
-    .create({
-      from: twilioNumber,
-      to: userNumber,
-      body: placeholder_message
-    })
-    .then(msg => console.log(msg.dateCreated))
-    .catch(err => console.error(err));
-}
-*/
 exports.default = router;
