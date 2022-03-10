20f363fa7a2554ccfd0816dc42aa9c2304704e16
ranonsew <ranonsew.2020@scis.smu.edu.sg>
2022-03-10 15:52:27 +0800

    rewriting a bit
    adding some stuff and removing some other things in order to make code more concise

--- index.ts
+++ index.ts
@@ -10,32 +10,25 @@ const msg_invalid = "Invalid URL";
 
 // environment variables and setting them up
 dotenv.config(); // pull .env file stuff into process.env
-// const accountSID = process.env.TWILIO_ACCOUNT_SID;
-// const authToken = process.env.TWILIO_AUTH_TOKEN;
-// const twilioNumber = process.env.TWILIO_PHONE_NUMBER;
-// const userNumber = process.env.PLACEHOLDER_PHONE_NUMBER;
 const {
   TWILIO_ACCOUNT_SID,
   TWILIO_AUTH_TOKEN,
-  TWILIO_PHONE_NUMBER,
-  PLACEHOLDER_PHONE_NUMBER
+  TWILIO_PHONE_NUMBER
 } = process.env; // object destructuring because it looks sort of cool
 
 // for main post request (sending message to the user)
 router.route('/')
   .post((req, res) => {
-    const { message } = req.body; // retrieving message from the request body
-    // if message isn't applied or is not of type string
-    if (!message || typeof message != "string") res.status(400).json(returnMsg("message in body has not been implemented properly"));
-    if (!TWILIO_ACCOUNT_SID || !TWILIO_AUTH_TOKEN || !TWILIO_PHONE_NUMBER || !PLACEHOLDER_PHONE_NUMBER) res.status(500).json(returnMsg("One or more environment variables are missing"));
-    // if (!accountSID || !authToken || !twilioNumber || !userNumber) res.status(500).json(returnMsg("One or more variables are missing"));
+    const { message, receiver } = req.body; // retrieving message from the request body
+    // if message or receiver isn't applied or is not of type string
+    if (!message || typeof message != "string" || !receiver || typeof receiver != "string") res.status(400).json(returnMsg("message in body has not been implemented properly"));
+    if (!TWILIO_ACCOUNT_SID || !TWILIO_AUTH_TOKEN || !TWILIO_PHONE_NUMBER) res.status(500).json(returnMsg("One or more environment variables are missing"));
     else {
       const twilioClient = new Twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
-      // const twilioClient = new Twilio(accountSID, authToken);
       twilioClient.messages
         .create({
           from: TWILIO_PHONE_NUMBER,
-          to: PLACEHOLDER_PHONE_NUMBER,
+          to: receiver,
           body: message
         })
         .then((msg) => res.status(200).json(returnMsg("Message has been sent to the user")))
