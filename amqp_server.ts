38cee89773b10b1986b0590529da26a0d845305b
ranonsew <ranonsew.2020@scis.smu.edu.sg>
2022-03-21 15:13:43 +0800

    Delete amqp_server.ts

--- amqp_server.ts
+++
@@ -1,7 +0,0 @@
-import amqp from "amqplib";
-const open = amqp.connect("amqp://localhost");
-open.then((connection) => connection.createChannel())
-  .then((channel) => {
-    channel.assertExchange("message_topic", "topic", { durable: true })
-  })
-  .catch(err => { throw err });
