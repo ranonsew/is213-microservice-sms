6c619ba0909c5052dd66f5f1685bc2469eb7c73d
ranonsew <ranonsew.2020@scis.smu.edu.sg>
2022-03-24 21:50:00 +0800

    Update amqp_index.ts
    updated with stuff from http index

--- amqp_index.ts
+++ amqp_index.ts
@@ -1,14 +1,24 @@
 // rabbitmq-mgmt container id: 0c476c4886f8
 // localhost:15672 -- rabbitmq
 
+import { Twilio } from "twilio";
+import dotenv from "dotenv";
 import amqp from "amqplib";
 
-// necessary variables
+// json err response message template
+const err_msg = (status: number, message: string) => ({ status, message });
+
+// environment variables for twilio
+dotenv.config(); // pull .env file stuff into process.env
+const { TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, TWILIO_PHONE_NUMBER} = process.env;
+// object destructuring because it looks sort of cool
+
+// variables for amqp
 const exchange = { name: 'message_topic', type: 'topic' };
 const options = { durable: true };
 const queue = { name: 'Twilio-Sms', key: '*.notification' };
 
-// open connection to rabbitmq
+// helper function: open connection to rabbitmq
 async function openChannel() {
   const connection = await amqp.connect({
     protocol: 'amqp',
@@ -21,7 +31,7 @@ async function openChannel() {
   return { channel, connection };
 };
 
-// close connection to rabbitmq
+// helper function: close connection to rabbitmq
 function closeCh(ch: amqp.Channel, conn: amqp.Connection) {
   setTimeout(() => {
     ch.close();
@@ -43,11 +53,25 @@ const send = async (jsonData: string) => {
 const receive = async () => {
   const { channel, connection } = await openChannel();
   await channel.assertQueue(queue.name, options);
-  await channel.consume(queue.name, msg => {
+  await channel.consume(queue.name, async msg => {
     if (!msg) return; // if null then don't send anything
-    const jsonData = msg.content.toString(); // storing first for some other use?
-      // TODO: access twilio sending function
-    console.log(jsonData); // console log string first, idk what to do
+    const { message, receiver } = JSON.parse(msg.content.toString()); // storing first for some other use?
+    try {
+      if (!message || typeof message != "string" || !receiver || typeof receiver != "string") throw err_msg(400, "One or more body variables have not been added.");
+      if (!TWILIO_ACCOUNT_SID || !TWILIO_AUTH_TOKEN || !TWILIO_PHONE_NUMBER) throw err_msg(500, "One or more environment variables are missing.");
+      const twilioClient = new Twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
+      const result = await twilioClient.messages.create({
+        from: TWILIO_PHONE_NUMBER,
+        to: receiver,
+        body: message
+      });
+      if (!result) throw err_msg(500, "Something went wrong while sending the message.");
+      console.log({ message: "Message has been sent to the user." });
+    } catch (err) {
+      console.log(err);
+    }
+
+    console.log(message, receiver); // console log string first, idk what to do
     channel.ack(msg); // acknowledge the message
     channel.cancel('myconsumer');
   }, { consumerTag: 'myconsumer' });
