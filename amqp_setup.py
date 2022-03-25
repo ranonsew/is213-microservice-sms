ce49058d9e038a590b5be9d5c566759e01f4db7b
ranonsew <ranonsew.2020@scis.smu.edu.sg>
2022-03-25 21:06:05 +0800

    Update amqp_setup.py

--- amqp_setup.py
+++ amqp_setup.py
@@ -1,46 +1,50 @@
 # python example is213 amqp for sending and receiving
 
 import pika
+import atexit
 
 exchange = { "name": "log_topic", "type": "topic" }
-log = {
+topics = {
   "error": { "name": "Error_Log", "key": "*.error" },
-  "activity": { "name": "Activity_Log", "key": "*.activity" }
+  "activity": { "name": "Activity_Log", "key": "*.activity" },
+  "notification": { "name": "Twilio-Sms", "key": "*.notification" }
+
 }
 notification = { "name": "Twilio-Sms", "key": "*.notification" }
 
-def openConnection():
-  connection = pika.BlockingConnection(
-    pika.ConnectionParameters(
-      host="localhost",
-      port="5672",
-      locale="en-US",
-      heartbeat=3600
-    )
-  )
-  channel = connection.channel()
-  channel.exchange_declare(exchange.get("name"), exchange.get("type"), durable=True)
-  return (channel, connection)
 
-def closeChannel(ch: BlockingChannel, conn: BlockingConnection):
-  ch.close()
-  conn.close()
+connection = pika.BlockingConnection(
+  pika.ConnectionParameters(
+    host="localhost",
+    port="5672",
+    locale="en-US",
+    heartbeat=3600
+  )
+)
+channel = connection.channel()
+channel.exchange_declare(exchange.get("name"), exchange.get("type"), durable=True)
+
+for topic in topics.keys():
+  queue_name = topics.get(topic).get("name")
+  key = topics.get(topic).get("key")
+  channel.queue_declare(queue=queue_name, durable=True)
+  channel.queue_bind(exchange=exchange.get("name"), queue=queue_name, routing_key=key)
+
+
 
 def sendLog():
-  channel, connection = openConnection()
-  for key in log.keys():
-    channel.queue_declare
   pass
 
 def callback():
   pass
 
-def receiveLog():
-  pass
-
-def sendMsg(jsonData: string):
-  channel, connection = openConnection()
+def sendMsg(jsonData):
   channel.queue_declare(queue=notification.get("name"), durable=True)
   channel.queue_bind(exchange=exchange.get("name"), queue=notification.get("name"), routing_key=notification.get("key"))
   channel.basic_publish(exchange=exchange.get("name"), routing_key=notification.get("key"), body=jsonData)
-  closeChannel(channel, connection)
+
+def closeChannel():
+  channel.close()
+  connection.close()
+
+atexit.register(closeChannel())
