0994ee8dc49a18780140d6645a1ae726552f2bc7
ranonsew <ranonsew.2020@scis.smu.edu.sg>
2022-03-26 03:24:05 +0800

    Update amqp_setup.py
    split into 2 exchanges, 1 for logging, 1 for message sending to the twilio microservice
    
    3 queues for the 3 things, error, activity, and notif

--- amqp_setup.py
+++ amqp_setup.py
@@ -2,17 +2,16 @@
 
 import pika
 import atexit
-# from flask import Flask, request, jsonify
-# import os, sys
-# import json
 
-# set up exchange types and topic variables
-exchange_name = "log_topic"
-exchange_type = "topic"
-topics = {
-  "error": { "name": "Error_Log", "key": "*.error" },
-  "activity": { "name": "Activity_Log", "key": "*.activity" },
-  "notification": { "name": "Twilio-Sms", "key": "*.notification" }
+
+# set up exchanges
+log_exchange = "log_topic"
+notif_exchange = "notif_topic"
+# setup queues
+queues = {
+  "error": { "name": "Error_Log", "key": "*.error", "exchange": log_exchange },
+  "activity": { "name": "Activity_Log", "key": "*.activity", "exchange": log_exchange },
+  "notif": { "name": "Twilio-Sms", "key": "#", "exchange": notif_exchange }
 }
 
 
@@ -26,28 +25,34 @@ connection = pika.BlockingConnection(
   )
 )
 channel = connection.channel()
-channel.exchange_declare(exchange_name, exchange_type, durable=True)
-for topic in topics.keys():
-  top = topics.get("topic")
-  channel.queue_declare(queue=top.get("name"), durable=True)
-  channel.queue_bind(exchange=exchange_name, queue=top.get("name"), routing_key=top.get("key"))
+# declare the log and notif exchanges
+channel.exchange_declare(exchange=log_exchange, exchange_type="topic", durable=True)
+channel.exchange_declare(exchange=notif_exchange, exchange_type="topic", durable=True)
 
 
-# helper functions for sending data using amqp
-def send(topic, data):
+# declare and bind the 3 queues, 2 to log exchanage, 1 to notif exchange
+for queue in queues.values():
+  channel.queue_declare(queue=queue.get("name"), durable=True)
+  channel.queue_bind(exchange=queue.get("exchange"), queue=queue.get("name"), routing_key=queue.get("key"))
+
+
+# helper functions for publishing data to amqp
+def send(exchange, queue, data):
   channel.basic_publish(
-    exchange=exchange_name,
-    routing_key=topic.get("key"),
-    body=data,
-    properties=pika.BasicProperties(delivery_mode = 2)
+    exchange=exchange,
+    routing_key=queue.get("key"),
+    body=data, # data: string, transformed into byte code
+    properties=pika.BasicProperties(delivery_mode = 2) # persistent
   )
-# flask stuff helper functions
+# helper function for sending an error log
 def sendErrorLog(error_log):
-  send(topics.get("error"), error_log)
+  send(log_exchange, queues.get("error"), error_log)
+# helper function for sending an activity log
 def sendActivityLog(activity_log):
-  send(topics.get("activity"), activity_log)
+  send(log_exchange, queues.get("activity"), activity_log)
+# helper function for sending an sms through Twilio
 def sendMsg(smsData):
-  send(topics.get("notification"), smsData)
+  send(notif_exchange, queues.get("notif"), smsData)
 
 
 # closing channel when process is ended
