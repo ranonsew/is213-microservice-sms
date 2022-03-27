63223f2cda350e9a5d0b370e378043eb9462bdf9
ranonsew <ranonsew.2020@scis.smu.edu.sg>
2022-03-27 20:28:46 +0800

    Update server.py
    updated host for docker

--- server.py
+++ server.py
@@ -90,4 +90,4 @@ def sendMessage():
 
 if __name__ == '__main__':
   print("this is flask " + os.path.basename(__file__) + " for Things")
-  app.run(host="127.0.0.1", port=5101, debug=True)
+  app.run(host="0.0.0.0", port=5101, debug=True)
