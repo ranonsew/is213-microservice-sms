31d9ce4f67a2fbf24d83f75548927f3f7e89fda0
ranonsew <ranonsew.2020@scis.smu.edu.sg>
2022-03-27 22:00:48 +0800

    Update amqp_setup.py

--- amqp_setup.py
+++ amqp_setup.py
@@ -2,8 +2,6 @@
 
 import pika
 from os import environ
-import atexit
-
 
 # set up exchanges
 log_exchange = "log_topic"
@@ -23,8 +21,9 @@ connection = pika.BlockingConnection(
   pika.ConnectionParameters(
     host=rabbitHost,
     port=rabbitPort,
-    locale="en-US",
-    heartbeat=3600
+    # locale="en-US",
+    heartbeat=3600,
+    blocked_connection_timeout=3600
   )
 )
 channel = connection.channel()
@@ -39,8 +38,37 @@ for queue in queues.values():
   channel.queue_bind(exchange=queue.get("exchange"), queue=queue.get("name"), routing_key=queue.get("key"))
 
 
+def is_connection_open(connection):
+  try:
+    connection.process_data_events()
+    return True
+  except pika.exceptions.AMQPError as e:
+    print("AMQP Error: ", e)
+    print("...creating new connection.")
+    return False
+
+def check_setup():
+  global connection, channel, rabbitHost, rabbitPort, log_exchange, notif_exchange
+  if not is_connection_open(connection):
+    connection = pika.BlockingConnection(
+      pika.ConnectionParameters(
+        host=rabbitHost,
+        port=rabbitPort,
+        # locale="en-US",
+        heartbeat=3600,
+        blocked_connection_timeout=3600
+      )
+    )
+  if channel.is_closed():
+    channel = connection.channel()
+    # declare the log and notif exchanges
+    channel.exchange_declare(exchange=log_exchange, exchange_type="topic", durable=True)
+    channel.exchange_declare(exchange=notif_exchange, exchange_type="topic", durable=True)
+
+
 # export: helper functions for publishing data to amqp
 def send(exchange, queue, data):
+  check_setup()
   channel.basic_publish(
     exchange=exchange,
     routing_key=queue.get("key"),
@@ -56,10 +84,3 @@ def sendActivityLog(activity_log):
 # helper function for sending an sms through Twilio
 def sendMsg(smsData):
   send(notif_exchange, queues.get("notif"), smsData)
-
-
-# closing channel when process is ended
-# def closeChannel():
-#   channel.close()
-#   connection.close()
-# atexit.register(closeChannel())
